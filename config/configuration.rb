79c31a341b9ec3e93ac11ff645e690a499479c15
anton <anton@localhost.localdomain>
2018-03-22 19:56:28 +0700

    starttls=false default

--- config/configuration.rb
+++ config/configuration.rb
@@ -2,6 +2,6 @@ DEBUG = false
 DOMAIN = 'localhost'
 JSONRPC_PORT = 12080
 SHA1_SALT = 'a6f519d72b910000'
-STARTTLS = true
+STARTTLS = false
 KEYSTORE = "keystore.jks"
 KEYSTORE_PASSWORD = "password"
